eafce6f53c8c162ff7a453faef2dda279331146c
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-06-09 20:50:05 -0400

    Create skinningSkillBooks.lua

---
+++ Contents/mods/main/media/lua/server/skinningSkillBooks.lua
@@ -0,0 +1,9 @@
+require 'XpSystem/XPSystem_SkillBook'
+
+SkillBook["Skinning"] = {}
+SkillBook["Skinning"].perk = Perks.Skinning
+SkillBook["Skinning"].maxMultiplier1 = 3
+SkillBook["Skinning"].maxMultiplier2 = 5
+SkillBook["Skinning"].maxMultiplier3 = 8
+SkillBook["Skinning"].maxMultiplier4 = 12
+SkillBook["Skinning"].maxMultiplier5 = 16
